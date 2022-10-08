cc3e8838be54cb3956da694276031dbf9af614f5
Mskauen <mathias.skauen.harestad@gmail.com>
2022-10-08 02:22:36 +0200

    Protocal for requests link changed

--- src/client/lib/http/requests.jsx
+++ src/client/lib/http/requests.jsx
@@ -1,13 +1,13 @@
 import {fetchJson} from "./http";
 
 const requests = {
-    getMovie: async (id) => await fetchJson(`http://api.tvmaze.com/search/shows?q=${id}`),
-    getMovieSummary: async (id) => await fetchJson(`http://api.tvmaze.com/shows/${id}`),
+    getMovie: async (id) => await fetchJson(`https://api.tvmaze.com/search/shows?q=${id}`),
+    getMovieSummary: async (id) => await fetchJson(`https://api.tvmaze.com/shows/${id}`),
     getMovieBackground: async (id) => await fetchJson(`https://api.tvmaze.com/shows/${id}/images`),
-    getRandomMovie: async () => await fetchJson("http://api.tvmaze.com/shows").then(
+    getRandomMovie: async () => await fetchJson("https://api.tvmaze.com/shows").then(
         res => res[Math.floor(Math.random() * res.length - 1)]
     ),
-    getAllMovies: async () => await fetchJson("http://api.tvmaze.com/shows")
+    getAllMovies: async () => await fetchJson("https://api.tvmaze.com/shows")
 };
 
 module.exports = {
